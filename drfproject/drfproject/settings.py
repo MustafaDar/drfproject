e582e00cd1cc06b7606643add1c817a2fadb905a
sunilkumar1996 <sunilrajputdev@gmail.com>
2021-04-21 19:39:53 +0530

    add postgres config

--- drfproject/drfproject/settings.py
+++ drfproject/drfproject/settings.py
@@ -90,10 +90,24 @@ WSGI_APPLICATION = 'drfproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+### Sqlite3
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+### Postgres
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "testdb",
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
